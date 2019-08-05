f93384550cd3eb61f8db3f4f5f20fbd7d7fe9733
int100 <me@int100.org>
2019-08-05 22:14:50 +0800

    实现 paper 功能

---
+++ papergirl.py
@@ -0,0 +1,33 @@
+import httpapi
+import random
+
+konachanApi = httpapi.HttpGetAPI("http://konachan.net")
+konachanPostApi = konachanApi.endpoint("/post.json")
+
+yandereApi = httpapi.HttpGetAPI("https://yande.re")
+yanderePostApi = yandereApi.endpoint("/post.json")
+
+async def paper(cls, phrase, message):
+    print("Call")
+
+    query = "order:random rating:safe score:>=120"
+
+    if phrase.get("~query", None):
+        query = "{} {}".format(phrase.get("~query", None), query)
+
+    limit = int(phrase.get("*number", 1))
+    limit = 10 if limit > 10 else limit
+
+    print(query)
+    api = konachanPostApi
+    rep = (await api.call({
+        "limit": limit,
+        "tags": query
+    })).json()
+
+    if len(rep) == 0:
+        await cls.sendMessage(message['type'], message['group'], message['qq'], "Not found")
+        return
+    
+    for i in rep:
+        await cls.sendMessage(message['type'], message['group'], message['qq'], cls.formatImage(i["file_url"]))

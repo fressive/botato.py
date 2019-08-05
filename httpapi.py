a89e1f87804b7fe1ad3dd1708475dcfb7fc5cc98
int100 <me@int100.org>
2019-08-05 22:14:27 +0800

    修改 HttpAPI 相关封装

--- httpapi.py
+++ httpapi.py
@@ -6,16 +6,29 @@ class HttpAPI:
     def __init__(self, url):
         self.url = url
 
-    def call(self):
+    async def call(self):
         raise NotImplementedError
 
     def endpoint(self, endpoint):
         return HttpAPI(self.url + endpoint)
 
 class HttpGetAPI(HttpAPI):
+
+    def __init__(self, url):
+        super().__init__(url)
+
     async def call(self, params = None, **kwargs):
         return await requests.get(self.url, params=params, **kwargs)
 
+    def endpoint(self, endpoint):
+        return HttpGetAPI(self.url + endpoint)
+
 class HttpPostAPI(HttpAPI):
+    def __init__(self, url):
+        super().__init__(url)
+
     async def call(self, data=None, json=None, **kwargs):
         return await requests.post(self.url, data, json, **kwargs)
+
+    def endpoint(self, endpoint):
+        return HttpPostAPI(self.url + endpoint)

295d18ff20d30a5c041001c8fe0738f0e1157e43
int100 <me@int100.org>
2019-08-06 00:21:03 +0800

    添加及实现 artist 功能

--- functions.py
+++ functions.py
@@ -1,15 +1,17 @@
 from commanding import Phrase
 from osu import recent, osudebug
-from papergirl import paper
+from papergirl import paper, artist
 
 functions = {
     "recent": recent,
     "osudebug": osudebug,
-    "paper": paper
+    "paper": paper,
+    "artist": artist
 }
 
 commands = [
     Phrase("help", ["help me"]),
+    Phrase("artist", ["artist", ["*img", "a"]]),
     Phrase("paper", ["paper"]),
     Phrase("paper", ["paper", ["~query", "a"]]),
     Phrase("paper", ["paper", ["~query", "a"], "x", ["*number", "1"]]),
@@ -21,5 +23,6 @@ commands = [
 
 regexes = {
     "string": r"(?<=\").*?(?=\")",
-    "number": r"\-?[0-9]+(\.[0-9]+)?"
+    "number": r"\-?[0-9]+(\.[0-9]+)?",
+    "img": r"\[qq:pic=(\S*)\]"
 }
--- papergirl.py
+++ papergirl.py
@@ -1,11 +1,14 @@
 import httpapi
-import random
+import re
+import config
 
 konachanApi = httpapi.HttpGetAPI("https://konachan.net")
 konachanPostApi = konachanApi.endpoint("/post.json")
 
 yandereApi = httpapi.HttpGetAPI("https://yande.re")
 yanderePostApi = yandereApi.endpoint("/post.json")
+
+saucenaoApi = httpapi.HttpGetAPI("https://saucenao.com/search.php")
 
 async def paper(cls, phrase, message):
     query = "order:random rating:safe score:>=120"
@@ -28,3 +31,58 @@ async def paper(cls, phrase, message):
     
     for i in rep:
         await cls.sendMessage(message['type'], message['group'], message['qq'], cls.formatImage(i["file_url"]))
+
+img_regex = re.compile(r"\[qq:pic=(\S*)\]")
+
+async def artist(cls, phrase, message):
+    img = phrase.get("*img", None)
+    
+    if not img:
+        await cls.sendMessage(message['type'], message['group'], message['qq'], "Input an image")
+        return
+
+    uuid = img_regex.findall(img)[0]
+    
+    imgurl = cls.getImageUrl(uuid)
+    sauce = (await saucenaoApi.call({
+        "output_type": 2,
+        "api_key": config.sacuenao_api_key,
+        "testmode": 1,
+        "db": 999,
+        "numres": 15,
+        "url": imgurl
+    })).json()
+
+    flag = False
+    response = "Not Found"
+    
+    if not sauce["results"] or len(sauce["results"]) == 0:
+        flag = True
+    else:
+        for i in sauce["results"]:
+            sim = float(i["header"]["similarity"])
+            if sim >= 80.0:
+                # if the similarity greater than 80%, 
+                # then it is usually the source
+                if i["data"]["pixiv_id"]:
+                    # from pixiv
+                    response = "Author: {}\nhttps://www.pixiv.net/member.php?id={}".format(i["data"]["member_name"], i["data"]["member_id"])
+                elif i["data"]["author_name"] and i["data"]["author_url"]:
+                    response = "Author: {}\n{}".format(i["data"]["author_name"], i["data"]["author_url"])
+
+                if i["data"]["ext_urls"]:
+                    response = "Source: {}\n\n{}".format(", ".join(i["data"]["ext_urls"]), response)
+                if i["data"]["title"]:
+                    response = "{}\n{}".format(i["data"]["title"], response)
+
+                response = "Match: {}\n\n{}".format(sim, response)
+                    
+                break
+        else:
+            flag = True
+    if flag:
+        pass 
+        # TODO: search from ascii2d
+
+    await cls.sendMessage(message['type'], message['group'], message['qq'], response)
+    
